b2c79e3ba53d52846800cfadb069fc77ad0efec5
AndLuna <contato.andreluna@gmail.com>
2022-12-19 10:50:40 -0300

    fim js fim projeto

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,7 +12,88 @@ const sidebarBtn = document.querySelector("[data-sidebar-btn]");
 // callback na funcionalidade do toggle sidebar p/ mobile 
 sidebarBtn.addEventListener("click", function () { elementToggleFunc(sidebar); });
 
+// -     ///////////////////  trabalhando com o portfolio
 
+// criando as variaveis de selecao
+const select = document.querySelector("[data-select]");
+const selectItems = document.querySelectorAll("[data-select-item]");
+const selectValue = document.querySelector("[data-selecct-value]");
+const filterBtn = document.querySelectorAll("[data-filter-btn]");
+
+select.addEventListener("click", function () { elementToggleFunc(this); });
+
+// Agora adicionando evento para todos os itens
+for (let i = 0; i < selectItems.length; i++) {
+  selectItems[i].addEventListener("click", function () {
+    //entao dar a funcao de escolher uma opcao no select:
+
+    let selectedValue = this.innerText.toLowerCase();
+    selectValue.innerText = this.innerText;
+    elementToggleFunc(select);
+    filterFunc(selectedValue);
+
+  });
+}
+// agora filtrando as variaveis dentro do select
+const filterItems = document.querySelectorAll("[data-filter-item]");
+
+const filterFunc = function (selectedValue) {
+
+  for (let i = 0; i < filterItems.length; i++) {
+         //entao se ao escolher o valor, filtro active
+    if (selectedValue === "all") {
+      filterItems[i].classList.add("active");
+    } else if (selectedValue === filterItems[i].dataset.category) {
+      filterItems[i].classList.add("active");
+    } else {
+      filterItems[i].classList.remove("active");
+    }
+
+  }
+
+}
+
+// agora adicionando a funcionalidade aos botoes de filtro em telas maiores
+let lastClickedBtn = filterBtn[0];
+
+for (let i = 0; i < filterBtn.length; i++) {
+       //filtro para evento click, acionar arrow function pras variaveis de selecao de valor
+  filterBtn[i].addEventListener("click", function () {
+
+    let selectedValue = this.innerText.toLowerCase();
+    selectValue.innerText = this.innerText;
+    filterFunc(selectedValue);
+
+    lastClickedBtn.classList.remove("active");
+    this.classList.add("active");
+    lastClickedBtn = this;
+
+  });
+
+}
+
+
+// -     ///////////////////   trabalhando com o form
+
+// Variaveis do formulario, pegando os data-form de cada elemento
+const form = document.querySelector("[data-form]");
+const formInputs = document.querySelectorAll("[data-form-input]");
+const formBtn = document.querySelector("[data-form-btn]");
+
+
+// adicionando um evento pra todos os inputs de form
+for (let i = 0; i < formInputs.length; i++) {
+  formInputs[i].addEventListener("input", function () {
+
+    // checando a validacao dos inputs pra clicar em enviar
+    if (form.checkValidity()) {
+      formBtn.removeAttribute("disabled");
+    } else {
+      formBtn.setAttribute("disabled", "");
+    }
+
+  });
+}
 
 // variaveis de navegacao da pagina, data-nav-link menu e data-page na article
 const navigationLinks = document.querySelectorAll("[data-nav-link]");
